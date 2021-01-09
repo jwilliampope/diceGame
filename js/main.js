dce4bf06a62e94f46b4b5dc4b6a77cc22fccf4e7
jwilliampope <jwilliampope@hotmail.com>
2021-01-09 17:44:23 -0500

    started js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,37 @@ let player2Score = 0
 let player1Turn = true
 
 // Create variables to store references to the necessary DOM nodes
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+// Establish click event for roll Btn -
+// log out a random number between 1 and 6 hint math floor math random
+
+rollBtn.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  // display dice Number
+  // use 'active' class to show whcih players turn is now
+  // hint: use this.classList.remove()add() methods/
+  // update message DOM node to state whose turn is now
+
+  if (player1Turn) {
+    player1Dice.textContent = randomNumber
+    player1Dice.classList.remove("active")
+    player2Dice.classList.add("active")
+    message.textContent = "Player 2 Turn"
+  } else {
+    player2Dice.textContent = randomNumber
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent = "Player 1 Turn"
+  }
+
+  player1Turn = !player1Turn
+})
